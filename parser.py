74b62a92da9f1118b50b09a3bfbbfb660107d11b
labicquette <komete17@gmail.com>
2021-11-24 11:06:04 +0100

    little update

--- parser.py
+++ parser.py
@@ -1,6 +1,24 @@
 import pandas as pd
+import re
 
-df = pd.read_csv("trame1.txt", sep='\s+',index_col=0, skip_blank_lines=True, header=None, dtype=str)
-for i in df:
-    print(df[i][20])
-print(df)
+df = pd.read_csv("trame1.txt", sep='\s+',index_col=0, skip_blank_lines=True, header=None, dtype=str, comment='#')
+
+#for i in df:
+#    print(df[][i])
+#print(df)
+
+f = open('trame1.txt')
+
+offset = 0
+
+for line in f: 
+    #match = re.findall("^[0-9a-f]{4} ([0-9a-f]{2} ?)*$",line)
+    exp = r'^([0-9a-f]{4})( [0-9a-f]{2})* ?#?.*$'
+    #exp2 = ' [0-9a-f]{2}'
+    match = re.findall(exp, line)
+    #match2 = re.findall(exp2, line)
+    print(match)
+    #print(match2)
+    #print(match.group(0))
+
+f.close()
